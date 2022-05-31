421f67be3ec2fb2a08741c9737ee87738f25b7d3
SergeyZotoff <dart-adwokat@yandex.ru>
2022-06-01 00:02:01 +0500

    remove static carsCount and create object carService

--- src/main/java/web/DAO/DaoCarsImpl.java
+++ src/main/java/web/DAO/DaoCarsImpl.java
@@ -1,6 +1,7 @@
 package web.DAO;
 
 import web.model.Car;
+import web.service.CarService;
 import web.service.CarServiceImpl;
 
 import java.util.ArrayList;
@@ -8,16 +9,19 @@ import java.util.List;
 
 public class DaoCarsImpl implements DaoCars{
 
+
+
     @Override
     public List<Car> getCarList() {
+        CarServiceImpl carService = new CarServiceImpl();
         int count = 5;
         List<Car> cars = new ArrayList<>();
         cars.add(new Car("Lada", "Granta", 2020));
-        cars.add(new Car("Lada", "Granta", 2019));
+        cars.add(new Car("Lada", "Granta", 2021));
         cars.add(new Car("Lada", "Granta", 2018));
-        cars.add(new Car("Lada", "Granta", 2017));
+        cars.add(new Car("Lada", "Granta", 2014));
         cars.add(new Car("Lada", "Granta", 2016));
-        cars = CarServiceImpl.carsCount(cars, count);
+        cars = carService.carsCount(cars, count);
         return cars;
     }
 }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -3,14 +3,13 @@ package web.service;
 import web.DAO.DaoCarsImpl;
 import web.model.Car;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 public class CarServiceImpl implements CarService {
 
     private final DaoCarsImpl carService = new DaoCarsImpl();
 
-    static public List<Car> carsCount(List<Car> cars, int number) {
+     public  List<Car> carsCount(List<Car> cars, int number) {
         if (number == 0 || number > 5) return cars;
         return cars.stream().limit(number).collect(Collectors.toList());
     }
